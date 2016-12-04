bd3c399ade00fb49187fc4fa6f406d688a51bd0b
Salvo 'LtWorf' Tomaselli <tiposchi@tiscali.it>
2016-12-04 12:19:54 +0100

    fixnames: print version string

--- src/pulputils/__init__.py
+++ src/pulputils/__init__.py
@@ -2,3 +2,14 @@ import gettext
 gettext.bindtextdomain('pulputils')
 gettext.textdomain('pulputils')
 _ = gettext.gettext
+
+VERSION = '1.0'
+
+VERBOSE_VERSION = '%(prog)s (pulputils) ' + VERSION + '''
+Copyright (C) 2016 Salvo Tomaselli
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+Written by Salvo Tomaselli.
+'''
